940029ae1fd3fef426c32ae567c4c61b2ebe7d8a
Timi Delad <timilehinogunlade@gmail.com>
2024-01-06 21:13:09 +0100

    continue working on the javascript

--- index.js
+++ index.js
@@ -61,18 +61,54 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function clickImgFunction() {
-    let dayInput = document.getElementById('day');
-    let monthInput = document.getElementById('month');
-    let yearInput = document.getElementById('year');
+let dayInput = document.getElementById('day');
+let monthInput = document.getElementById('month');
+let yearInput = document.getElementById('year');
 
-    let dayInputConverted = parseInt(dayInput.input);
-    let monthInputConverted = parseInt(monthInput.input);
-    let yearInputConverted = parseInt(yearInput.input);
+let dayInputConverted = parseInt(dayInput.value);
+let monthInputConverted = parseInt(monthInput.value);
+let yearInputConverted = parseInt(yearInput.value);
 
-    const d = new Date();
+const d = new Date();
 
-    let currentDay = d.getDay();
+let currentYear = d.getFullYear();
+let currentMonth = d.getMonth() + 1;
+let currentDay = d.getDate();
+console.log(currentDay)
+
+function getDaysInMonth(month, year) {
+    return new Date(year, month + 1, 0).getDate();
 }
 
-document.getElementById('clickImg').onclick = clickImgFunction;
+function calculateAge(yearInputConverted, monthInputConverted, dayInputConverted, currentYear, currentMonth, currentDay) {
+
+    let years = currentYear - yearInputConverted;
+    let months = currentMonth - monthInputConverted;
+    let days = currentDay - dayInputConverted;
+
+    if (days < 0) {
+
+        const daysInPreviousMonth = getDaysInMonth(currentMonth - 2, currentYear);
+        days += daysInPreviousMonth;
+
+        months -= 1;
+    }
+
+    if (months < 0) {
+        months += 12;
+        
+        years -= 1;
+    }
+
+    return { years, months, days };
+}
+
+let age = calculateAge(yearInputConverted, monthInputConverted, dayInputConverted, currentYear, currentMonth, currentDay);
+
+function clickImgFunction() {
+
+    if((dayInputConverted == "" || dayInputConverted == " ") || (monthInputConverted == "" || monthInputConverted == " ") (yearInputConverted == "" || yearInputConverted == " ")) {
+        document.getElementById('errorMessage')
+    }
+}
+// document.getElementById('clickImg').onclick = clickImgFunction;
